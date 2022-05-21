37fdc590b0e475058e040937a9d543ac66e09f4c
DavidSavvy <dspuzzling@gmail.com>
2022-05-21 19:10:25 +0000

    Sat, May 21, 2022, 7:10 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -2,9 +2,12 @@
 #include <stdio.h>
 #include <string.h>
 
+int count_letters(string input);
+
 int main(void)
 {
     string input = get_string("Text: ");
+    count_letters(input);
 }
 
 int count_letters(string input)
@@ -12,6 +15,11 @@ int count_letters(string input)
     int letters = 0;
     for (int i = 0, i < strlen(input), i++) //might bring issue wit \0
     {
-        if 
+        if (input[i] > 64 && input[i] < 91 || input[i] > 96 && input[i] < 123) //might bring issue with mixing && and ||
+        {
+            letters++;
+        }
     }
+    return letters;
+    printf("%i", letters);
 }
