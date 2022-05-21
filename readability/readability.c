0cb714ebc735a9fec8c17f8e8c3afe2025facb42
DavidSavvy <dspuzzling@gmail.com>
2022-05-21 19:16:22 +0000

    Sat, May 21, 2022, 7:16 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -15,7 +15,7 @@ int count_letters(string input)
     int letters = 0;
     for (int i = 0; i < strlen(input); i++) //might bring issue wit \0
     {
-        if ((input[i] > 64 && input[i] < 91) || (input[i] > 96 && input[i] < 123)) 
+        if ((input[i] > 64 && input[i] < 91) || (input[i] > 96 && input[i] < 123))
         {
             letters++;
         }
