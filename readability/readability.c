3b3ef6f614ea9a2cd282d7387b19a242ecfca144
DavidSavvy <dspuzzling@gmail.com>
2022-05-21 19:05:12 +0000

    Sat, May 21, 2022, 7:05 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,7 +1,14 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
-int main(void) 
+int main(void)
 {
+    string input = get_string("Text: ");
+}
 
+int count_letters(string input)
+{
+    int letters = 0;
+    for (int i = 0, )
 }
