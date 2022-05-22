2ce3a5f5bf29715b111f5c4350358ee332c47e74
DavidSavvy <dspuzzling@gmail.com>
2022-05-22 02:39:30 +0000

    Sun, May 22, 2022, 2:39 AM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -9,32 +9,40 @@ int main(int argc, string argv[])
 {
     if (argc != 2)
     {
-        printf("Usage ./substitution key");
+        printf("Usage ./substitution key\n");
         return 1;
     }
 
     string key = argv[1];
     if (strlen(key) != 26)
     {
-        printf("Key must contain 26 characters.");
+        printf("Key must contain 26 characters.\n");
     }
 
     string plainText = get_string("plain text: ");
 
     string cipherText = substitute(key, plainText);
 
-    printf("ciphertext: %s", cipherText);
+    printf("ciphertext: %s\n", cipherText);
 }
 
-string substitute(string key, string plainText)
+string substitute(string key, string plainText) //problem with space and comma
 {
     for (int i = 0; i < strlen(plainText); i++)
     {
         char temp = plainText[i];
 
         if (isupper(plainText[i]))
-
+        {
+            plainText[i] = key[i - 65];
+        }
+        else
+        {
+            plainText[i] = key[i - 97];
+        }
     }
+    return plainText;
+
 }
 
 /*
