134403724737cc4b1edbdd15d1b72b626e73c88f
DavidSavvy <dspuzzling@gmail.com>
2022-05-27 21:17:15 +0000

    Fri, May 27, 2022, 9:17 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -236,7 +236,7 @@ void sort_pairs(void)
         switchCount = 0;
         for (int i = 0; i < pair_count - 1; i++)
         {
-            if (preferences[pairs[i].winner][pairs[i].loser] > preferences[pairs[i + 1].winner][pairs[i].loser])
+            if (preferences[pairs[i].winner][pairs[i].loser] > preferences[pairs[i + 1].winner][pairs[i + 1].loser])
             {
                 pair temp = pairs[i];
                 pairs[i] = pairs[i + 1];
