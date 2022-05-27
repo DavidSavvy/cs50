a566357549da6a8cf14188b3acbf55a39bea358b
DavidSavvy <dspuzzling@gmail.com>
2022-05-27 00:16:12 +0000

    Fri, May 27, 2022, 12:16 AM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -122,7 +122,7 @@ void record_preferences(int ranks[])
         {
             if (i != ranks[j])
             {
-                preferences[i][j]++;
+                preferences[j][i]++;
             }
             else
             {
