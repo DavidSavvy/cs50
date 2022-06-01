f28ada0171a6644b0d992036ea3dd680ce844212
DavidSavvy <dspuzzling@gmail.com>
2022-06-01 02:36:19 +0000

    Wed, Jun 1, 2022, 2:36 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -141,7 +141,13 @@ bool vote(int voter, int rank, string name)
 // Tabulate votes for non-eliminated candidates
 void tabulate(void)
 {
-    // TODO
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (!candidates[i].elimated)
+        {
+            
+        }
+    }
     return;
 }
 
