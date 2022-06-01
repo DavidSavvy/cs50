4bf5a444d2993116effeb89474124702f810b195
DavidSavvy <dspuzzling@gmail.com>
2022-06-01 03:15:35 +0000

    Wed, Jun 1, 2022, 3:15 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -1,5 +1,6 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 // Max voters and candidates
 #define MAX_VOTERS 100
@@ -129,7 +130,7 @@ bool vote(int voter, int rank, string name)
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        if (candidates[i].name == name)
+        if (strcmp(candidates[i].name, name) == 0)
         {
             preferences[voter][rank] = i;
             return true;
