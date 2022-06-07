bf25a21e499ecbe4d00c03ba7fe12292c310ccb4
DavidSavvy <dspuzzling@gmail.com>
2022-06-07 22:01:34 +0000

    Tue, Jun 7, 2022, 10:01 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -10,9 +10,9 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width; j++)
         {
             avg = (image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed)/3;
-            images[i][j].rgbtBlue = avg;
-            images[i][j].rgbtGreen = avg;
-            images[i][j].rgbtRed = avg;
+            image[i][j].rgbtBlue = avg;
+            image[i][j].rgbtGreen = avg;
+            image[i][j].rgbtRed = avg;
         }
     }
     return;
@@ -25,9 +25,9 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            images[i][j].rgbtBlue = .272 * images[i][j].rgbtRed + .534 * images[i][j].rgbtGreen + .131 * images[i][j].rgbtBlue;
-            images[i][j].rgbtGreen = .349 * images[i][j].rgbtRed + .686 * images[i][j].rgbtGreen + .168 * images[i][j].rgbtBlue;
-            images[i][j].rgbtRed = .393 * images[i][j].rgbtRed + .769 * images[i][j].rgbtGreen + .189 * images[i][j].rgbtBlue;
+            image[i][j].rgbtBlue = .272 * image[i][j].rgbtRed + .534 * image[i][j].rgbtGreen + .131 * image[i][j].rgbtBlue;
+            image[i][j].rgbtGreen = .349 * image[i][j].rgbtRed + .686 * image[i][j].rgbtGreen + .168 * image[i][j].rgbtBlue;
+            image[i][j].rgbtRed = .393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen + .189 * image[i][j].rgbtBlue;
         }
     }
     return;
@@ -36,11 +36,26 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    BYTE tempBlue;
+    BYTE tempGreen;
+    BYTE tempRed;
     for (int i = 0; i < height; i++)
     {
-        for (int j = 0; j < width; j++)
+        for (int j = 0, k = width - 1; j < k; j++)
         {
+            tempBlue = image[i][j].rgbtBlue;
+            image[i][j].rgbtBlue = image[i][k].rgbtBlue;
+            image[i][k].rgbtBlue = tempBlue;
 
+            tempGreen = image[i][j].rgbtGreen;
+            image[i][j].rgbtGreen = image[i][k].rgbtGreen;
+            image[i][k].rgbtGreen = tempGreen;
+
+            tempRed = image[i][j].rgbtRed;
+            image[i][j].rgbtRed = image[i][k].rgbtRed;
+            image[i][k].rgbtRed = tempRed;
+
+            k--;
         }
     }
     return;
