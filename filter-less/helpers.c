96eccee2ce16bb539c60f755e134c2482508dbb1
DavidSavvy <dspuzzling@gmail.com>
2022-06-07 23:33:17 +0000

    Tue, Jun 7, 2022, 11:33 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -25,9 +25,9 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            image[i][j].rgbtBlue = fmin(round(.272 * image[i][j].rgbtRed + .534 * image[i][j].rgbtGreen + .131 * image[i][j].rgbtBlue), 255);
-            image[i][j].rgbtGreen = fmin(round(.349 * image[i][j].rgbtRed + .686 * image[i][j].rgbtGreen + .168 * image[i][j].rgbtBlue), 255);
-            image[i][j].rgbtRed = fmin(round(.393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen + .189 * image[i][j].rgbtBlue), 255);
+            image[i][j].rgbtBlue = min(round(.272 * image[i][j].rgbtRed + .534 * image[i][j].rgbtGreen + .131 * image[i][j].rgbtBlue), 255);
+            image[i][j].rgbtGreen = min(round(.349 * image[i][j].rgbtRed + .686 * image[i][j].rgbtGreen + .168 * image[i][j].rgbtBlue), 255);
+            image[i][j].rgbtRed = min(round(.393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen + .189 * image[i][j].rgbtBlue), 255);
         }
     }
     return;
