40d189e2cb8f8846d25ff9994933ce25286880fb
DavidSavvy <dspuzzling@gmail.com>
2022-06-07 23:41:27 +0000

    Tue, Jun 7, 2022, 11:41 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -25,9 +25,9 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            image[i][j].rgbtBlue = (BYTE)fmin(round((int).272 * image[i][j].rgbtRed + .534 * image[i][j].rgbtGreen + .131 * image[i][j].rgbtBlue), 255);
-            image[i][j].rgbtGreen = (BYTE)fmin(round((int).349 * image[i][j].rgbtRed + .686 * image[i][j].rgbtGreen + .168 * image[i][j].rgbtBlue), 255);
-            image[i][j].rgbtRed = (BYTE)fmin(round((int).393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen + .189 * image[i][j].rgbtBlue), 255);
+            image[i][j].rgbtBlue = (BYTE)fmin((int)(.272 * image[i][j].rgbtRed + .534 * image[i][j].rgbtGreen + .131 * image[i][j].rgbtBlue), 255);
+            image[i][j].rgbtGreen = (BYTE)fmin((int)(.349 * image[i][j].rgbtRed + .686 * image[i][j].rgbtGreen + .168 * image[i][j].rgbtBlue), 255);
+            image[i][j].rgbtRed = (BYTE)fmin((int)(.393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen + .189 * image[i][j].rgbtBlue), 255);
         }
     }
     return;
