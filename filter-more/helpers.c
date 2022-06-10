ffaf86aab14abf5e56c11a36d1ff28394027af19
DavidSavvy <dspuzzling@gmail.com>
2022-06-10 00:22:55 +0000

    Fri, Jun 10, 2022, 12:22 AM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -89,9 +89,9 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
             sobRed = 0;
             sobGreen = 0;
             sobBlue = 0;
-            for (int row = i - 1; row <= i + 1; row++)
+            for (int row = i - 1; row < i + 2; row++)
             {
-                for (int col = j - 1; col <= i + 1; col++)
+                for (int col = j - 1; col < i + 2; col++)
                 {
                     if (row > -1 && row < height && col > -1 && col < width)
                     {
