899d8b8c16bc59129d036a79171992c76ae2f56c
DavidSavvy <dspuzzling@gmail.com>
2022-06-13 20:47:13 +0000

    Mon, Jun 13, 2022, 8:47 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -22,12 +22,65 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+
+    for (int row = 0; row < height; row++)
+    {
+        for (int i = 0, j = width - 1; i < j; i++)
+        {
+            BYTE tempR = image[row][i].rgbtRed;
+            BYTE tempG = image[row][i].rgbtGreen;
+            BYTE tempB = image[row][i].rgbtBlue;
+
+            image[row][i].rgbtRed = image[row][j].rgbtRed;
+            image[row][j].rgbtRed = tempR;
+
+            image[row][i].rgbtGreen = image[row][j].rgbtGreen;
+            image[row][j].rgbtGreen = tempG;
+
+            image[row][i].rgbtBlue = image[row][j].rgbtBlue;
+            image[row][j].rgbtBlue = tempB;
+
+            j--;
+        }
+    }
     return;
 }
 
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    RGBTRIPLE imageCpy[height][width];
+    memcpy(imageCpy, image, sizeof(RGBTRIPLE) * (width * height));
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int avgR = 0;
+            int avgG = 0;
+            int avgB = 0;
+            int count = 0;
+            for (int row = i - 1; row < i + 1; row++)
+            {
+                for (int col = j - 1; col < j + 1; col++)
+                {
+                    if (row > -1 && row < height && col > -1 && col < width)
+                    {
+                        avgR += imageCpy[row][col].rgbtRed;
+                        avgG += imageCpy[row][col].rgbtGreen;
+                        avgB += imageCpy[row][col].rgbtBlue;
+                        count++;
+                    }
+                }
+            }
+            avgR /= count;
+            avgG /= count;
+            avgB /= count;
+            image[i][j].rgbtRed = avgR;
+            image[i][j].rgbtGreen = avgG;
+            image[i][j].rgbtBlue = avgB;
+        }
+    }
     return;
 }
 
