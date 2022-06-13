8db49e20adbb96dc1c2add0dcaef11e4a2f2c2a9
DavidSavvy <dspuzzling@gmail.com>
2022-06-13 22:35:15 +0000

    Mon, Jun 13, 2022, 10:35 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -5,7 +5,7 @@ int main(int argc, char *argv[])
 {
     typedef uint8_t BYTE;
     const int BLOCKSIZE = 512;
-    int jpgN = -1;
+    int jpgN = 0;
     if (argc != 2)
     {
         printf("Usage: ./recover infile.raw");
@@ -28,6 +28,7 @@ int main(int argc, char *argv[])
     {
         if (buffer[0] == 0xFF && buffer[1] == 0xD8 && buffer[2] == 0xFF && (buffer[3] & 0xF0) == 0xE0)
         {
+            
             jpgN++;
             isJPG = !isJPG;
             if (!isJPG)
