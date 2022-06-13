559edac6524dc781669420472f5646ce96fc4bd3
DavidSavvy <dspuzzling@gmail.com>
2022-06-13 23:34:32 +0000

    Mon, Jun 13, 2022, 11:34 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -30,14 +30,16 @@ int main(int argc, char *argv[])
     int isJPG = 0;
     while (fread(buffer, sizeof(BYTE), BLOCKSIZE, file) == BLOCKSIZE)
     {
-        printf("%li\n", ftell(file));
+        //printf("%li\n", ftell(file));
         if (buffer[0] == 0xFF && buffer[1] == 0xD8 && buffer[2] == 0xFF && (buffer[3] & 0xF0) == 0xE0)
         {
             if (!isJPG)
             {
-                printf("%li\n", ftell(file));
+
+                //printf("%li\n", ftell(file));
                 sprintf(name, "%03i.jpg", jpgN);
                 image = fopen(name, "w");
+                fwrite(buffer, sizeof(BYTE), BLOCKSIZE, image);
                 fwrite(file, sizeof(BYTE), BLOCKSIZE, image); //might need to take next block
                 jpgN++;
                 isJPG = 1;
