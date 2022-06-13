21d5c35de351fa9b4f90f99c03e421e169b2232c
DavidSavvy <dspuzzling@gmail.com>
2022-06-13 23:52:30 +0000

    Mon, Jun 13, 2022, 11:52 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -7,24 +7,25 @@ int main(int argc, char *argv[])
     typedef uint8_t BYTE;
     const int BLOCKSIZE = 512;
     int jpgN = 0;
-    
+
     if (argc != 2)
     {
         printf("Usage: ./recover infile.raw");
         return 1;
     }
+
     FILE *file = fopen(argv[1], "r");
     if (file == NULL)
     {
         printf("Invalid file");
         return 1;
     }
+
     BYTE *buffer = malloc(BLOCKSIZE);
     if (buffer == NULL)
     {
         return 1;
     }
-
     char *name = malloc(8);
     FILE *image = NULL;
     int isJPG = 0;
