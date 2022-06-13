b369e3d279d4863e59503ad5b8d2a164550eeefe
DavidSavvy <dspuzzling@gmail.com>
2022-06-13 21:46:20 +0000

    Mon, Jun 13, 2022, 9:46 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -6,5 +6,11 @@ int main(int argc, char *argv[])
     if (argc != 2)
     {
         printf("Usage: ./recover infile.raw");
+        return 1;
+    }
+    FILE *file = fopen(argv[1], "r");
+    if (*file == NULL)
+    {
+        
     }
 }
