cfec94d806d3f34c414f030de5efed27ba1c2c65
DavidSavvy <dspuzzling@gmail.com>
2022-06-20 22:01:36 +0000

    Mon, Jun 20, 2022, 10:01 PM +00:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -19,7 +19,8 @@ def main():
     file = open(sys.argv[1], "r")
     reader = csv.DictReader(file)
 
-    for row in reader
+    for row in reader:
+        teams.append(row)
 
     counts = {}
     # TODO: Simulate N tournaments and keep track of win counts
