32c1d15f9567402c40a8ecf1083ec3d4730b6179
DavidSavvy <dspuzzling@gmail.com>
2022-06-25 19:51:17 +0000

    Sat, Jun 25, 2022, 7:51 PM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -9,9 +9,17 @@ def main():
     w = get_words(text)
     s = get_sentences(text)
 
+    l = l / w * 100
+    s = s / w * 100
+
     grade = round(0.0588 * l - 0.296 * s - 15.8)
 
-    print(f"Grade {grade}")
+    if grade < 1:
+        print("Before Grade 1")
+    elif grade > 16:
+        print("Grade 16+")
+    else:
+        print(f"Grade {grade}")
 
 def get_letters(text):
     letters = 0
@@ -30,5 +38,10 @@ def get_words(text):
 def get_sentences(text):
     sentences = 0
     for i in text:
-        if i in ("!", "?", ".")
+        if i in {"!", "?", "."}:
+            sentences += 1
+    return sentences
+
+if __name__ == "__main__":
+    main()
 
