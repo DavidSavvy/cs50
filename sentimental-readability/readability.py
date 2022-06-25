dfbbe474949279e33cf67e8b1582c17f82a2c75a
DavidSavvy <dspuzzling@gmail.com>
2022-06-25 19:44:06 +0000

    Sat, Jun 25, 2022, 7:44 PM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,2 +1,28 @@
 from cs50 import get_string
 
+
+
+def main():
+    text = get_string("Text: ")
+
+    l = get_letters(text)
+    w = get_words(text)
+    s = get_sentences(text)
+
+    grade = round(0.0588 * l - 0.296 * s - 15.8)
+
+    print(f"Grade {grade}")
+
+def get_letters(text):
+    letters = 0
+    for i in text:
+        if i.isalpha():
+            letters += 1
+    return letters
+
+def get_words(text):
+    words = 0
+    for i in text:
+        if i == " ":
+            
+
