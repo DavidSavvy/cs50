7f86c2be51c8bc1d5a319813abe1df75e2806935
DavidSavvy <dspuzzling@gmail.com>
2022-06-25 19:52:32 +0000

    Sat, Jun 25, 2022, 7:52 PM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,7 +1,6 @@
 from cs50 import get_string
 
 
-
 def main():
     text = get_string("Text: ")
 
@@ -21,12 +20,14 @@ def main():
     else:
         print(f"Grade {grade}")
 
+
 def get_letters(text):
     letters = 0
     for i in text:
         if i.isalpha():
             letters += 1
     return letters
+
 
 def get_words(text):
     words = 0
@@ -35,12 +36,14 @@ def get_words(text):
             words += 1
     return words + 1
 
+
 def get_sentences(text):
     sentences = 0
     for i in text:
         if i in {"!", "?", "."}:
             sentences += 1
     return sentences
+
 
 if __name__ == "__main__":
     main()
