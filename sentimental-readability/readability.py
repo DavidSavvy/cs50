0ceae0abc7bafc4e4295ce1d0842faa1bc845386
DavidSavvy <dspuzzling@gmail.com>
2022-06-25 19:46:42 +0000

    Sat, Jun 25, 2022, 7:46 PM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -24,5 +24,11 @@ def get_words(text):
     words = 0
     for i in text:
         if i == " ":
-            
+            words += 1
+    return words + 1
+
+def get_sentences(text):
+    sentences = 0
+    for i in text:
+        if i in ("!", "?", ".")
 
