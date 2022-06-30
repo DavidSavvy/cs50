e862e8c02ed6b2b73575249c30b6b831d9798044
DavidSavvy <dspuzzling@gmail.com>
2022-06-30 23:46:12 +0000

    Thu, Jun 30, 2022, 11:46 PM +00:00

--- movies/6.sql
+++ movies/6.sql
@@ -1,1 +1,5 @@
-SELECT AVG(rating) FROM ratings JOIN movies ON ratings.movie_id = movies.id WHERE year = 2012;
+SELECT AVG(rating)
+  FROM ratings
+       JOIN movies
+       ON ratings.movie_id = movies.id
+ WHERE year = 2012;
