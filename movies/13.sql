896b012188586ef5b2a884f3b5ab04d5f5b2d41d
DavidSavvy <dspuzzling@gmail.com>
2022-06-30 23:51:17 +0000

    Thu, Jun 30, 2022, 11:51 PM +00:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,1 +1,12 @@
-SELECT DISTINCT name FROM people JOIN stars ON people.id = stars.person_id WHERE stars.movie_id IN (SELECT movie_id FROM stars JOIN people ON stars.person_id = people.id WHERE people.name = "Kevin Bacon" AND people.birth = 1958) AND NOT name = "Kevin Bacon";
+SELECT DISTINCT name
+  FROM people
+       JOIN stars
+       ON people.id = stars.person_id
+ WHERE stars.movie_id IN
+       (SELECT movie_id
+          FROM stars
+               JOIN people
+               ON stars.person_id = people.id
+         WHERE people.name = "Kevin Bacon"
+           AND people.birth = 1958)
+   AND NOT name = "Kevin Bacon";
