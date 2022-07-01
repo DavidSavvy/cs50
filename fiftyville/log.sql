6c3195d8ce6c51f347327e408ef2c81c11444c5b
DavidSavvy <dspuzzling@gmail.com>
2022-07-01 22:29:57 +0000

    Fri, Jul 1, 2022, 10:29 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -22,3 +22,13 @@ SELECT name
   FROM people
        JOIN bank_accounts
          ON people.id = bank_accounts.person_id
+
+       JOIN 
+ WHERE bank_accounts.account_number IN
+       (SELECT account_number
+          FROM atm_transactions
+         WHERE year = 2021
+           AND month = 7
+           AND day = 28
+           AND atm_location = "Leggett Street"
+           AND transaction_type = "withdraw")
