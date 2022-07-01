2002790d8384616529e69e1465e3197324e247b4
DavidSavvy <dspuzzling@gmail.com>
2022-07-01 22:23:37 +0000

    Fri, Jul 1, 2022, 10:23 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -13,7 +13,7 @@ SELECT transcript FROM interviews WHERE year = 2021 AND month = 7 AND day = 28;
 SELECT license_plate FROM bakery_security_logs WHERE year = 2021 AND month = 7 AND day = 28 AND hour = 10 AND minute > 15 AND minute < 25 AND activity = "exit";
 
 --Returns participants in calls under a minute long like the interviewee mentioned
-SELECT caller, receiver, duration  FROM phone_calls WHERE year = 2021 AND month = 7 AND day = 28 AND duration < 60;
+SELECT caller, receiver, duration FROM phone_calls WHERE year = 2021 AND month = 7 AND day = 28 AND duration < 60;
 
 --Returns account numbers that withdrew from Leggett Street as interviewee mentioned
 SELECT account_number FROM atm_transactions WHERE year = 2021 AND month = 7 AND day = 28 AND atm_location = "Leggett Street" AND transaction_type = "withdraw";
