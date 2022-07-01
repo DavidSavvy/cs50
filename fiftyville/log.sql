621a4d384013fe851081e43824729dc202db23bf
DavidSavvy <dspuzzling@gmail.com>
2022-07-01 22:22:42 +0000

    Fri, Jul 1, 2022, 10:22 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -10,7 +10,7 @@ SELECT id, description FROM crime_scene_reports WHERE year = 2021 AND month = 7 
 SELECT transcript FROM interviews WHERE year = 2021 AND month = 7 AND day = 28;
 
 --Returns license plate and allows me to see people who left the parking lot within the time the interviewee mentioned
-SELECT license_plate, minute, activity FROM bakery_security_logs WHERE year = 2021 AND month = 7 AND day = 28 AND hour = 10;
+SELECT license_plate FROM bakery_security_logs WHERE year = 2021 AND month = 7 AND day = 28 AND hour = 10 AND minute > 15 AND minute < 25 AND activity = "exit";
 
 --Returns participants in calls under a minute long like the interviewee mentioned
 SELECT caller, receiver, duration  FROM phone_calls WHERE year = 2021 AND month = 7 AND day = 28 AND duration < 60;
