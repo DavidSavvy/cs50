cb9241d769960d9b6a4cf36348f841c371c8bc56
DavidSavvy <dspuzzling@gmail.com>
2022-07-01 23:16:05 +0000

    Fri, Jul 1, 2022, 11:16 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,25 +1,59 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
 -- Returns description of crime and mentions bakery as well as three witnesses
-SELECT id, description FROM crime_scene_reports WHERE year = 2021 AND month = 7 AND day = 28 AND street = "Humphrey Street";
+SELECT id, description
+  FROM crime_scene_reports
+ WHERE year = 2021
+   AND month = 7
+   AND day = 28
+   AND street = "Humphrey Street";
 
 --Returns various transcripts WITH KEY INFORMATION:
 --Thief withdrew ATM money on Leggett Street
 --Thief got into car at bakery parking lot within 10 minutes of the theft
 --Thief made a call and said they were taking the earliest flight next morning, asked the other person to purchase the ticket (accomplice)
-SELECT transcript FROM interviews WHERE year = 2021 AND month = 7 AND day = 28;
+SELECT transcript
+  FROM interviews
+ WHERE year = 2021
+   AND month = 7
+   AND day = 28;
 
 --Returns license plate and allows me to see people who left the parking lot within the time the interviewee mentioned
-SELECT license_plate FROM bakery_security_logs WHERE year = 2021 AND month = 7 AND day = 28 AND hour = 10 AND minute > 15 AND minute < 25 AND activity = "exit";
+SELECT license_plate
+  FROM bakery_security_logs
+ WHERE year = 2021
+   AND month = 7
+   AND day = 28
+   AND hour = 10
+   AND minute > 15
+   AND minute < 25
+   AND activity = "exit";
 
 --Returns participants in calls under a minute long like the interviewee mentioned
-SELECT caller, receiver, duration FROM phone_calls WHERE year = 2021 AND month = 7 AND day = 28 AND duration < 60;
+SELECT caller, receiver, duration
+  FROM phone_calls
+ WHERE year = 2021
+   AND month = 7
+   AND day = 28
+   AND duration < 60;
 
 --Returns account numbers that withdrew from Leggett Street as interviewee mentioned
-SELECT account_number FROM atm_transactions WHERE year = 2021 AND month = 7 AND day = 28 AND atm_location = "Leggett Street" AND transaction_type = "withdraw";
+SELECT account_number
+  FROM atm_transactions
+ WHERE year = 2021
+   AND month = 7
+   AND day = 28
+   AND atm_location = "Leggett Street"
+   AND transaction_type = "withdraw";
 
 --Returns flight id of earliest flight from Fiftyville the day after the crime
-SELECT id FROM flights WHERE year = 2021 AND month = 7 AND day = 29 ORDER BY hour LIMIT 1;
+SELECT id
+  FROM flights
+ WHERE year = 2021
+   AND month = 7
+   AND day = 29
+ ORDER BY hour
+ LIMIT 1;
 
 --Returns person who matches all the queries above (Thief):
 --Left bakery lot within 10 minutes of crime
