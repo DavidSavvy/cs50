a53ef151a2b503b8fd1d840066c0cf15610ebcf5
DavidSavvy <dspuzzling@gmail.com>
2022-07-01 21:46:38 +0000

    Fri, Jul 1, 2022, 9:46 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,2 +1,6 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
+-- Returns description of crime and mentions bakery as well as three witnesses
+SELECT id, description FROM crime_scene_reports WHERE year = 2021 AND month = 7 AND day = 28 AND street = "Humphrey Street";
+
+SELECT license_plate FROM bakery_security_logs WHERE year = 2021 AND month = 7 AND day = 28 AND hour = 10;
