72364ce3f27a8c165863a35fc52763e7939a7687
DavidSavvy <dspuzzling@gmail.com>
2022-07-15 00:48:04 +0000

    Fri, Jul 15, 2022, 12:48 AM +00:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -31,6 +31,8 @@ def index():
         # TODO: Add the user's entry into the database
         if not request.form.get("name") or request.form.get("month") not in MONTHS or request.form.get("day") not in DAYS:
             return redirect("/")
+        else:
+            db.execute("INSERT )
         return redirect("/")
 
     else:
