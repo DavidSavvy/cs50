41d2d7c8caf99e1a11ea0201931906c4da07a16c
DavidSavvy <dspuzzling@gmail.com>
2022-07-15 01:01:11 +0000

    Fri, Jul 15, 2022, 1:01 AM +00:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -34,7 +34,7 @@ def index():
             return redirect("/")
         else:
             db.execute("INSERT INTO birthdays (name, month, day) VALUES (?, ?, ?)", (request.form.get("name"), request.form.get("month"), request.form.get("day")))
-            
+            return render_template("index.html", all_birthdays=all_birthdays, months=MONTHS, days=DAYS)
         return redirect("/")
 
     else:
