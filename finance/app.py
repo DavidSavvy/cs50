f1b8cdfc63e9ab51da10a3379d54cd283bf1f5cf
DavidSavvy <dspuzzling@gmail.com>
2022-07-21 23:26:51 +0000

    Thu, Jul 21, 2022, 11:26 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -219,7 +219,16 @@ def register():
         elif password != confirmation:
             return apology("passwords must match")
 
-        reg = re.fullmatch(r"^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\w\d\s:])([^\s]){8,16}$")
+        """
+        password must contain 1 number (0-9)
+        password must contain 1 uppercase letters
+        password must contain 1 lowercase letters
+        password must contain 1 non-alpha numeric number
+        password is 8-16 characters with no space
+        """
+        reg = re.fullmatch(r"^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\w\d\s:])([^\s]){8,16}$", password)
+        if not reg:
+            return apology("invalid password")
 
         hash = generate_password_hash(password, method='pbkdf2:sha256', salt_length=8)
 
