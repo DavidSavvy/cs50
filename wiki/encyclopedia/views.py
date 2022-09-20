e12a111a40a7a86d9e8a1f622ba2d840e0ceb7f7
DavidSavvy <dspuzzling@gmail.com>
2022-09-20 12:57:26 +0000

    Tue, Sep 20, 2022, 12:57 PM +00:00

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import Http404
+from django.http import Http404, HttpResponse
 
 import markdown2
 
@@ -23,4 +23,4 @@ def page(request, title):
         })
 
 def search(request, search):
-    return HttpsResponse 
+    return HttpResponse("hello")
