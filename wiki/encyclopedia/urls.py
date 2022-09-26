7dc5667219baf648ffa165d0b666bd08ea5b9de9
DavidSavvy <dspuzzling@gmail.com>
2022-09-26 12:59:22 +0000

    Mon, Sep 26, 2022, 12:59 PM +00:00

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path("wiki/<str:title>", views.page, name="page"),
     path("wiki", views.search, name="search"),
     path("create", views.create, name="create"),
-    path("edit/<str:title>", views.edit, name="edit")
+    path("wiki/edit/<str:title>", views.edit, name="edit")
 ]
