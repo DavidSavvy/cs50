92c52bd76230c20a0eae536c35c63beecb4bb802
DavidSavvy <dspuzzling@gmail.com>
2022-10-05 13:21:46 +0000

    Wed, Oct 5, 2022, 1:21 PM +00:00

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -7,7 +7,7 @@ class User(AbstractUser):
 
 class Listing(models.Model):
     listing_id = models.IntegerField(primary_key=True)
-    price = models.DecimalField(decimal_places=2)
+    price = models.DecimalField(decimal_places=2, max_digits=10)
     description = models.TextField()
     category = models.CharField(max_length=64)
     lister = models.ForeignKey(User, on_delete=models.CASCADE, related_name='listings')
@@ -15,7 +15,7 @@ class Listing(models.Model):
 
 class Bid(models.Model):
     bid_id = models.IntegerField(primary_key=True)
-    bid = models.DecimalField(decimal_places=2)
+    bid = models.DecimalField(decimal_places=2, max_digits=10)
     bid_item = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name='item_bids')
     bidder = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_bids')
 
