aa638300b0d5686c83482be7119af588ba8805a0
DavidSavvy <dspuzzling@gmail.com>
2022-10-05 13:01:41 +0000

    Wed, Oct 5, 2022, 1:01 PM +00:00

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -6,7 +6,7 @@ class User(AbstractUser):
     pass
 
 class Listing(models.Model):
-    listing_id = models.PrimaryKey()
+    listing_id = models.IntegerField(primary_key=True)
     price = models.DecimalField(decimal_places=2)
     category = models.CharField(max_length=64)
     lister = models.ForeignKey
