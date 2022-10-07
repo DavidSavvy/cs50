a7bc0c3fb63538061c8da546c33fb92251d6bd1b
DavidSavvy <dspuzzling@gmail.com>
2022-10-07 01:22:10 +0000

    Fri, Oct 7, 2022, 1:22 AM +00:00

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -6,6 +6,7 @@ class User(AbstractUser):
     pass
 
 class Listing(models.Model):
+    listing_id = models.AutoField(primary_key=True)
     price = models.DecimalField(decimal_places=2, max_digits=10)
     description = models.TextField()
     category = models.CharField(max_length=64)
@@ -13,11 +14,13 @@ class Listing(models.Model):
     date = models.DateTimeField()
 
 class Bid(models.Model):
+    bid_id = models.AutoField(primary_key=True)
     bid = models.DecimalField(decimal_places=2, max_digits=10)
     bid_item = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name='item_bids')
     bidder = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_bids')
 
 class Comment(models.Model):
+    comment_id = models.AutoField(primary_key=True)
     text = models.TextField()
     commenter = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_comments')
     comment_item = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name='item_comments')
