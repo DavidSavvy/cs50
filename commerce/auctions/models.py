022f53c2d9243e12fe5c4c174fe457c850f2ba48
DavidSavvy <dspuzzling@gmail.com>
2022-10-07 12:46:14 +0000

    Fri, Oct 7, 2022, 12:46 PM +00:00

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -24,7 +24,3 @@ class Comment(models.Model):
     text = models.TextField()
     commenter = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_comments')
     comment_item = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name='item_comments')
-
-"""
-foreign key mistmatch bug
-"""
