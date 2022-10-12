40a5ba548e5f009489a35459f36facb48ba06578
DavidSavvy <dspuzzling@gmail.com>
2022-10-12 22:57:39 +0000

    Wed, Oct 12, 2022, 10:57 PM +00:00

--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -10,7 +10,7 @@ from .models import *
 
 def index(request):
     return render(request, "auctions/index.html", {
-        "listings": Listing.objects.all
+        "listings": Listing.objects.all()
     })
 
 @csrf_exempt
