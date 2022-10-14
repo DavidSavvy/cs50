2f5044982bf127ee3f7c7926212966991a89f58f
DavidSavvy <dspuzzling@gmail.com>
2022-10-14 13:08:04 +0000

    Fri, Oct 14, 2022, 1:08 PM +00:00

--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -36,7 +36,7 @@ def create(request):
             listing.image_url = image_url if (image_url != None) else None
             listing.save()
 
-            return index(request)
+            return HttpResponseRedirect(reverse())
 
 @csrf_protect
 def login_view(request):
