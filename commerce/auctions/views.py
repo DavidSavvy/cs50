285fbb1a310cc44e5cced434f6c7d69e07f65e30
DavidSavvy <dspuzzling@gmail.com>
2022-10-18 12:56:55 +0000

    Tue, Oct 18, 2022, 12:56 PM +00:00

--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -89,3 +89,7 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+
+def listing(request):
+    if request.method == "GET":
+        return render(request, ")
