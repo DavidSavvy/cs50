9f80597d1336a24caf74e3fb4a46a55961858093
DavidSavvy <dspuzzling@gmail.com>
2022-10-31 13:08:43 +0000

    Mon, Oct 31, 2022, 1:08 PM +00:00

--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -117,8 +117,7 @@ def listing(request, id, is_bid_valid=True):
         if Bid.objects.filter(bid_item=listing).order_by("-bid")[0].bidder == request.user:
             current_bidder = True
 
-    is_lister = False
-    (is_lister = True) if listing.lister == request.user else False
+    (is_lister := True) if listing.lister == request.user else False
     return render(request, "auctions/listing.html", {
         "listing": listing,
         "is_lister": is_lister,
