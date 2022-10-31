07a836f31b109d6ac861cbb6ae77d79330f551eb
DavidSavvy <dspuzzling@gmail.com>
2022-10-31 13:10:42 +0000

    Mon, Oct 31, 2022, 1:10 PM +00:00

--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -82,7 +82,7 @@ def logout_view(request):
     logout(request)
     return HttpResponseRedirect(reverse("index"))
 
-
+@csrf_protect
 def register(request):
     if request.method == "POST":
         username = request.POST["username"]
