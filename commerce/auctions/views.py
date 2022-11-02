b52eac27e8cdf00a2258989d6b0e7958a4629e0e
DavidSavvy <dspuzzling@gmail.com>
2022-11-02 13:02:17 +0000

    Wed, Nov 2, 2022, 1:02 PM +00:00

--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -41,7 +41,7 @@ def create(request):
 
 def close(request, id):
     listing = Listing.objects.get(listing_id=id)
-    bid = listing.item_bids.
+    bid = listing.item_bids.order_by()
     #Listing.objects.filter(listing_id=id).delete()
     return index(request)
 
