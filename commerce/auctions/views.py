3d4f420489612b366c122a90c8cc4cbfe3aa4eec
DavidSavvy <dspuzzling@gmail.com>
2022-11-04 13:13:18 +0000

    Fri, Nov 4, 2022, 1:13 PM +00:00

--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -55,7 +55,7 @@ def close(request, id):
 @csrf_exempt
 @login_required
 def comment(request, id):
-    comment_text = request.POST["comment_text"]
+    comment_text = request.POST["comment_text"][0]
     current_listing = Listing.objects.get(listing_id=id)
     current_comment = Comment.objects.create(text=comment_text, commenter=request.user, comment_item=current_listing)
     current_comment.save()
