0c19727cdece280bc133663cc03ec19541008344
DavidSavvy <dspuzzling@gmail.com>
2022-11-04 13:14:46 +0000

    Fri, Nov 4, 2022, 1:14 PM +00:00

--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -55,11 +55,13 @@ def close(request, id):
 @csrf_exempt
 @login_required
 def comment(request, id):
-    comment_text = request.POST["comment_text"][0]
-    current_listing = Listing.objects.get(listing_id=id)
-    current_comment = Comment.objects.create(text=comment_text, commenter=request.user, comment_item=current_listing)
-    current_comment.save()
-    return HttpResponseRedirect(reverse('listing'), kwargs={'id': id})
+    if request.method == "POST":
+
+        comment_text = request.POST["comment_text"][0]
+        current_listing = Listing.objects.get(listing_id=id)
+        current_comment = Comment.objects.create(text=comment_text, commenter=request.user, comment_item=current_listing)
+        current_comment.save()
+        return HttpResponseRedirect(reverse('listing'), kwargs={'id': id})
 
 @csrf_exempt
 @login_required
