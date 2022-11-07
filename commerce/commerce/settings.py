b63f6185c40bd87ab5ac9e2e3615863b4cebb43a
DavidSavvy <dspuzzling@gmail.com>
2022-11-07 13:57:47 +0000

    Mon, Nov 7, 2022, 1:57 PM +00:00

--- commerce/commerce/settings.py
+++ commerce/commerce/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'http')
 CSRF_COOKIE_SECURE=False
 CSRF_TRUSTED_ORIGINS=['https://davidsavvy-code50-103088195-pjqpqwgp7fr9v-8000.preview.app.github.dev/']
 
