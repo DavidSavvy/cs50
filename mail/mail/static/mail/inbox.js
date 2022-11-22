fe5b7a189bd16afb1fbc8550675a3827d860a81c
DavidSavvy <dspuzzling@gmail.com>
2022-11-22 13:47:16 +0000

    Tue, Nov 22, 2022, 1:47 PM +00:00

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -58,13 +58,13 @@ function load_mailbox(mailbox) {
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 
-  if (mailbox === 'sent'){
-    fetch('/emails/sent')
-    .then(response => response.json())
-    .then(emails => {
-      console.log(emails);
-  });
-  }
+
+  fetch('/emails/sent')
+  .then(response => response.json())
+  .then(emails => {
+    console.log(emails);
+  })
+
 }
 
 /*
