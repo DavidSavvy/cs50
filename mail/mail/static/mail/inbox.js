e5112f587a66d511496eef0be36cc97d7b160fee
DavidSavvy <dspuzzling@gmail.com>
2022-11-30 14:15:04 +0000

    Wed, Nov 30, 2022, 2:15 PM +00:00

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -23,7 +23,7 @@ function compose_email(email = null) {
     document.querySelector('#compose-body').value = '';
   } else {
     document.querySelector('#compose-recipients').value = email["sender"];
-    document.querySelector('#compose-subject').value = `Re: ${email["subject"]}` ? document.querySelector('#compose-subject').value === '' : email["subject"];
+    document.querySelector('#compose-subject').value = `Re: ${email["subject"]}` ? (document.querySelector('#compose-subject').value.slice(0,2) !== 'Re:') : email["subject"];
     document.querySelector('#compose-body').value = `On ${email['timestamp']}, ${email['sender']} wrote: \n${email["body"]}`;
   }
 
