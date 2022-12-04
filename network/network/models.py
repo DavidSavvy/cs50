31864697262126d7fc26745072dcce5555cb8568
DavidSavvy <dspuzzling@gmail.com>
2022-12-04 02:54:07 +0000

    Sun, Dec 4, 2022, 2:54 AM +00:00

--- network/network/models.py
+++ network/network/models.py
@@ -3,7 +3,8 @@ from django.db import models
 
 
 class User(AbstractUser):
-    following = models.ManyToManyField()
+    following = models.ManyToManyField("self")
+    followers = models.ManyToManyField("self")
 
 class Post(models.Model):
     poster = models.ForeignKey(User, on_delete=models.CASCADE ,related_name="posts")
