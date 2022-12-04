535318d55f7e5f70ad664f36620d88bfb1e7116c
DavidSavvy <dspuzzling@gmail.com>
2022-12-04 03:54:44 +0000

    Sun, Dec 4, 2022, 3:54 AM +00:00

--- network/network/models.py
+++ network/network/models.py
@@ -3,10 +3,12 @@ from django.db import models
 
 
 class User(AbstractUser):
+    user_id = models.AutoField(primary_key=True)
     following = models.ManyToManyField("self")
     followers = models.ManyToManyField("self")
 
 class Post(models.Model):
+    post_id = models.AutoField(primary_key=True)
     poster = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
     likers = models.ManyToManyField(User, related_name="liked_posts")
     timestamp = models.DateTimeField(auto_now_add=True)
