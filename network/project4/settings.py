eedc09a3abad08f9e1ecd4df429e8df2451193b7
DavidSavvy <dspuzzling@gmail.com>
2022-12-05 13:39:23 +0000

    Mon, Dec 5, 2022, 1:39 PM +00:00

--- network/project4/settings.py
+++ network/project4/settings.py
@@ -86,6 +86,8 @@ AUTH_USER_MODEL = "network.User"
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
+
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
