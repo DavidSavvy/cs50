2483135e29ab49d5d1055e23f3bd96d73e7775ba
DavidSavvy <dspuzzling@gmail.com>
2022-12-05 14:15:33 +0000

    Mon, Dec 5, 2022, 2:15 PM +00:00

--- network/network/static/network/network.js
+++ network/network/static/network/network.js
@@ -1,9 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-    for (let post in Document.querySelectorAll("#post")) {
-        if (!post.getElementById("#edit_link")) {
-            post.style.marginBottom = "0px";
-        }
-    }
 
   });
