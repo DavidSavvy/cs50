00cb64f89e6d9d099b9180606b2e75ae3d0589ea
DavidSavvy <dspuzzling@gmail.com>
2022-12-09 13:56:08 +0000

    Fri, Dec 9, 2022, 1:56 PM +00:00

--- network/network/views.py
+++ network/network/views.py
@@ -4,13 +4,17 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 
 from .models import User, Post
 
 
 def index(request):
+    posts = Post.objects.all()
+    paginator = Paginator(posts, 10)
     return render(request, "network/index.html", {
-        "posts": Post.objects.all()
+        "posts": posts,
+        "page_obj": page_obj
     })
 
 @login_required
