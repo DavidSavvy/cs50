18625897f372b37063e3b6a62f4a4577ff942eb7
DavidSavvy <dspuzzling@gmail.com>
2022-12-13 14:02:23 +0000

    Tue, Dec 13, 2022, 2:02 PM +00:00

--- network/network/static/network/network.js
+++ network/network/static/network/network.js
@@ -1,14 +1,6 @@
 function test(event){
   alert('test');
-  event.target.innerHTML = "
-  <div class="post" style="height: 250px; padding: 15px 15px 15px 15px;">
-            <h4 style="">New Post</h4>
-            <form method="POST" action="/post">
-                {% csrf_token %}
-                <textarea rows="5" style="width: 100%;" name="body"></textarea>
-                <br>
-                <input style="" class="btn btn-primary" type="submit" value="Post">
-            </form>
-        </div>";
+  
+  event.target.innerHTML = "";
 }
 
