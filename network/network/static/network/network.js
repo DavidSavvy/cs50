536f10ddb75861ac69a7afc9736a465abf7fa1a1
DavidSavvy <dspuzzling@gmail.com>
2022-12-22 18:07:47 +0000

    Thu, Dec 22, 2022, 6:07 PM +00:00

--- network/network/static/network/network.js
+++ network/network/static/network/network.js
@@ -5,14 +5,13 @@ import ReactDOM from 'react-dom';
 document.addEventListener('DOMContentLoaded', () => {
 
 })
+function Testfunc(event){
+
+  ReactDOM.render(<Edit/>, event.target.parentElement);
+}
+
 
 class EditMessage extends React.Component {
-  function Testfunc(event){
-    //alert('test');
-
-    //event.target.parentElement.innerHTML = "";
-    ReactDOM.render(<Edit/>, event.target.parentElement);
-  }
 
   function Edit(){
     return (
