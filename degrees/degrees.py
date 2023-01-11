15b3dce1926c27f456e8b641a73d8eb8ff9c401f
DavidSavvy <dspuzzling@gmail.com>
2023-01-11 13:43:18 +0000

    Wed, Jan 11, 2023, 1:43 PM +00:00

--- degrees/degrees.py
+++ degrees/degrees.py
@@ -99,10 +99,11 @@ def shortest_path(source, target):
     while not queue_frontier.empty():
         print(queue_frontier.frontier)
         working_node = queue_frontier.remove()
+        print(working_node)
         if working_node in explored_nodes:
             continue
         relations = list(neighbors_for_person(working_node.state))
-        print(relations)
+        #print(relations)
         for star_set in relations:
             star_set = list(star_set)
             #print(star_set)
@@ -136,7 +137,7 @@ def shortest_path(source, target):
                 """
             queue_frontier.add(node)
         explored_nodes.append(working_node)
-        print(explored_nodes)
+        #print(explored_nodes)
         starting_node = working_node
     return None
 
